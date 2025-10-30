27ebfce31d3b612320b472a837953833b873f626
lindseyebia <lebia@seattleu.edu>
2025-10-30 15:59:42 -0700

    Added Google Sign-In

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { Homepage } from './app/homepage/homepage';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())]
 });
+bootstrapApplication(Homepage, {
+  providers: [
+    provideHttpClient(withFetch()),
+    provideRouter([
+      { path: '', component: Homepage },
+      { path: '**', redirectTo: '' }
+    ])
+  ]
+}).catch(err => console.error(err));
