047b7224eac515c0daf69cd9028c22aafbfeda7b
Kirzyx <ryankluo@gmail.com>
2025-12-02 12:07:23 -0800

    fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+import { Homepage } from './app/homepage/homepage';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+});
+bootstrapApplication(Homepage, {
+  providers: [
+    provideHttpClient(withFetch()),
+    provideRouter([
+      { path: '', component: Homepage },
+      { path: '**', redirectTo: '' }
+    ])
+  ]
+}).catch(err => console.error(err));
